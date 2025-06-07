6333cb958cd17a871ba9f6fc1eca2497f87d7bf8
Cesar Febres <febrescesar7@gmail.com>
2025-06-07 15:26:48 -0400

    better way to group the api routes

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,23 @@ use App\Http\Controllers\Api\WeatherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function ($router) {
+// Rutas públicas de autenticación
+Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('register', 'register');
 });
 
-
+// Rutas protegidas por el middleware de autenticación
 Route::middleware('custom-sanctum-auth')->group(function () {
-    Route::post('get-weather-by-city', [WeatherController::class, 'getWeatherByCity']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('recent-searches', [WeatherController::class, 'getRecentSearches']);
-    Route::get('favorites', [WeatherController::class, 'getFavorites']);
-    Route::post('toggle-favorite', [WeatherController::class, 'toggleFavorite']);
+    
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('logout', 'logout');
+    });
+
+    Route::controller(WeatherController::class)->group(function () {
+        Route::post('get-weather-by-city', 'getWeatherByCity');
+        Route::get('recent-searches', 'getRecentSearches');
+        Route::get('favorites', 'getFavorites');
+        Route::post('toggle-favorite', 'toggleFavorite');
+    });
 });
-
-
